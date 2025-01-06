4cb7898a715ee696807d77f14489b619f052babe
樊炳希 <1366599892@qq.com>
2025-01-06 20:33:02 +0800

    2025.1.6 20:32

---
+++ Source/LearningGA/LearningGAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearningGAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALearningGAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALearningGAGameMode();
+};
+
+
+
